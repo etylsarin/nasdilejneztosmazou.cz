6a01fea31ff02b954f32b441d1de98cd2d800c05
Filip Mares <fmares@sessionm.com>
2022-03-08 23:57:52 +0100

    finalization

--- src/components/section-layout.tsx
+++ src/components/section-layout.tsx
@@ -1,17 +1,12 @@
 
 import * as React from "react";
 import * as styles from "./section-layout.module.scss";
-import { StaticImage } from "gatsby-plugin-image";
 
-export const SectionLayout = ({ children, ...args }) => {
-  console.log(args);
+export const SectionLayout = ({ children }) => {
   return (
   <section className={styles.section}>
     <div className={styles.text}>
       {children}
-    </div>
-    <div className={styles.image}>
-      {/* <StaticImage src={pageContext.frontmatter.imageSrc} alt={pageContext.frontmatter.imageAlt} /> */}
     </div>
   </section>
 )};
